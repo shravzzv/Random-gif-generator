ccdfaf49c2e87850f6b77881612931843564771f
sai shravan <saishravan384@gmail.com>
2023-10-16 10:53:15 +0530

    feat: Add error handling

--- script.js
+++ script.js
@@ -6,16 +6,29 @@ const API_KEY = 'uDu77HHFhXrEpWJCGuI83p9YtVnbhQAf'
 // free api key
 
 const generateSrc = () => {
+  loader.style.display = 'inline-block'
+  img.style.display = 'none'
   fetch(`${URL}?api_key=${API_KEY}`, {
     mode: 'cors',
   })
-    .then((res) => res.json())
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error('Network response was not OK')
+      }
+      return res.json()
+    })
     .then((res) => {
       img.src = res.data.images.original.url
       img.style.display = 'block'
+    })
+    .catch((err) => {
+      console.error('Error fetching data:', err)
+      window.alert(err.message)
+      img.style.display = 'none'
+    })
+    .finally(() => {
       loader.style.display = 'none'
     })
-    .catch((err) => console.log(err))
 }
 
 const radomize = (e) => {
