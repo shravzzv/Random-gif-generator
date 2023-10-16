65a4c6f4fc9282adc7d5a9f2198f01cb28987c66
sai shravan <saishravan384@gmail.com>
2023-10-16 14:54:25 +0530

    refactor: Use asycn await with generateSrc

--- script.js
+++ script.js
@@ -6,32 +6,24 @@ const search = document.querySelector('input')
 const API_KEY = 'uDu77HHFhXrEpWJCGuI83p9YtVnbhQAf'
 // free api key
 
-const generateSrc = () => {
+const generateSrc = async () => {
   const URL = 'https://api.giphy.com/v1/gifs/random'
+
   loader.style.display = 'inline-block'
   img.style.display = 'none'
 
-  fetch(`${URL}?api_key=${API_KEY}`, {
-    mode: 'cors',
-  })
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error('Network response was not OK')
-      }
-      return res.json()
-    })
-    .then((res) => {
-      img.src = res.data.images.original.url
-    })
-    .catch((err) => {
-      window.alert('An error occured! Check your internet connection.')
-      console.error(err)
-    })
-    .finally(() => {
-      loader.style.display = 'none'
-      // in case of an error, previous gif continues displaying
-      img.style.display = 'block'
-    })
+  try {
+    const res = await fetch(`${URL}?api_key=${API_KEY}`)
+    if (!res.ok) throw new Error('Network response was not OK')
+    const data = await res.json()
+    img.src = data.data.images.original.url
+  } catch (err) {
+    window.alert('An error occured! Check your internet connection.')
+    console.error(err)
+  }
+
+  loader.style.display = 'none'
+  img.style.display = 'block'
 }
 
 const radomize = (e) => {
